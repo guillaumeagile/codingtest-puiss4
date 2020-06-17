2a9f643ce8b1d5d6dfb2f62bf50ee62582c9099d
4m4d3us <59365596+4m4d3us@users.noreply.github.com>
2020-06-17 23:07:40 +0200

    Initialisation structure
    Création des structures de bases représentant le jeux.
    EtatCase pour représenté l'état d'une case du plateau
    Game représentant le plateau de jeux

---
+++ puissance4.src/EtatCase.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace puissance4.src
+{
+
+    /// <summary>
+    /// Représente l'état d'une case du plateau de jeu
+    /// </summary>
+    public enum EtatCase
+    {
+        Vide,
+        Jaune,
+        Rouge
+    }
+}
--- puissance4.src/Game.cs
+++ puissance4.src/Game.cs
@@ -1,10 +1,62 @@
 ﻿using System;
 
 namespace puissance4.src
-{
-
-    
+{        
+    /// <summary>
+    /// Représentation du plateau de jeu
+    /// </summary>
     public class Game
     {
+        public const int NB_LIGNES = 6;
+        public const int NB_COLONNES = 7;
+        private EtatCase[,] cases = new EtatCase[NB_COLONNES, NB_LIGNES];
+
+        /// <summary>
+        /// Démarre une nouvelle partie
+        /// </summary>
+        public Game()
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Reprend sur une partie existante
+        /// </summary>
+        /// <param name="jeu"></param>
+        public Game(EtatCase[,] jeu)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Méthode permettant de connaitre le vainqueur de la partie
+        /// </summary>
+        /// <returns>Vide, si pas de vainqueur, sinon la couleur du vaninqueur</returns>
+        public EtatCase Vainqueur()
+        {
+            throw new NotImplementedException("Vainqueur");
+        }
+
+        /// <summary>
+        /// Retourne l'état d'une 
+        /// </summary>
+        /// <param name="ligne"></param>
+        /// <param name="colonne"></param>
+        /// <returns></returns>
+        public EtatCase EtatCase(int colonne, int ligne)
+        {
+            throw new NotImplementedException("EtatCase");
+        }
+
+        /// <summary>
+        /// Un joueur joue un jeton dans une colonne
+        /// </summary>
+        /// <param name="joueur">Couleur du joueur</param>
+        /// <param name="colonne">Colonne jouée</param>
+        /// <returns>true si coup vainqueur</returns>
+        public bool Jouer(EtatCase joueur, int colonne)
+        {
+            throw new NotImplementedException("EtatCase");
+        }
     }
 }
