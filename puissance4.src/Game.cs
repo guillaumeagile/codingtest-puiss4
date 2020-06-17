f5bf1f4c97da2c8b87f80609015276a5cbd2361c
4m4d3us <59365596+4m4d3us@users.noreply.github.com>
2020-06-17 23:52:03 +0200

    Ajout test Vainqueur

--- puissance4.src/Game.cs
+++ puissance4.src/Game.cs
@@ -35,10 +35,46 @@ namespace puissance4.src
         /// <summary>
         /// Méthode permettant de connaitre le vainqueur de la partie
         /// </summary>
-        /// <returns>Vide, si pas de vainqueur, sinon la couleur du vaninqueur</returns>
+        /// <returns>Vide, si pas de vainqueur, sinon la couleur du vainqueur</returns>
         public EtatCase Vainqueur()
         {
-            throw new NotImplementedException("Vainqueur");
+            for (int i = 0; i < NB_LIGNES; i++)
+                for (int j = 0; j < NB_COLONNES; j++)
+                {
+                    if (cases[i, j] == src.EtatCase.Vide) continue;
+                    if (gagnant(i, j)) return cases[i, j];
+                }
+            return src.EtatCase.Vide;
+        }
+
+        private bool gagnant(int ligne, int colonne)
+        {
+            return
+                //test ligne gauche
+                (colonne - 3 >= 0 && cases[ligne, colonne] == cases[ligne, colonne - 1] && cases[ligne, colonne] == cases[ligne, colonne - 2] && cases[ligne, colonne] == cases[ligne, colonne - 3])
+                ||
+                //test ligne droite
+                (colonne + 3 < NB_COLONNES && cases[ligne, colonne] == cases[ligne, colonne + 1] && cases[ligne, colonne] == cases[ligne, colonne + 2] && cases[ligne, colonne] == cases[ligne, colonne + 3])
+                ||
+                //test colonne bas
+                (ligne - 3 >= 0 && cases[ligne, colonne] == cases[ligne -1 , colonne] && cases[ligne, colonne] == cases[ligne -1, colonne] && cases[ligne, colonne] == cases[ligne-3, colonne ])
+                ||
+                //test colonne haut
+                (ligne + 3 < NB_LIGNES && cases[ligne, colonne] == cases[ligne + 1, colonne] && cases[ligne, colonne] == cases[ligne + 2, colonne] && cases[ligne, colonne] == cases[ligne + 3, colonne])
+                ||
+                //test diagonale 1
+                (ligne + 3 < NB_LIGNES && colonne + 3 < NB_COLONNES && cases[ligne, colonne] == cases[ligne + 1, colonne+1] && cases[ligne, colonne] == cases[ligne + 2, colonne+2] && cases[ligne, colonne] == cases[ligne + 3, colonne+3])
+                ||
+                //test diagonale 2
+                (ligne + 3 < NB_LIGNES && colonne - 3 >= 0 && cases[ligne, colonne] == cases[ligne + 1, colonne - 1] && cases[ligne, colonne] == cases[ligne + 2, colonne - 2] && cases[ligne, colonne] == cases[ligne + 3, colonne - 3])
+                ||
+                //test diagonale 3
+                (ligne - 3 >= 0 && colonne + 3 < NB_COLONNES && cases[ligne, colonne] == cases[ligne - 1, colonne + 1] && cases[ligne, colonne] == cases[ligne - 2, colonne + 2] && cases[ligne, colonne] == cases[ligne - 3, colonne + 3])
+                                ||
+                //test diagonale 4
+                (ligne - 3 >= 0 && colonne - 3 >= 0 && cases[ligne, colonne] == cases[ligne - 1, colonne - 1] && cases[ligne, colonne] == cases[ligne - 2, colonne - 2] && cases[ligne, colonne] == cases[ligne - 3, colonne - 3])
+            ;
+
         }
 
         /// <summary>
