138482bc15a9230988be5eb6d4b35256868c93e8
4m4d3us <59365596+4m4d3us@users.noreply.github.com>
2020-06-17 23:27:26 +0200

    Premier Tests Unitaires
    Test de l'instanciation de la partie.
    Test de la reprise de la partie.
    Test du vainqueur d'une partie reprise

---
+++ Puissance4.Tests/GameTest.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using Xunit;
+using puissance4.src;
+
+namespace Puissance4.Tests
+{
+    public class GameTest
+    {
+        private EtatCase[,] partieTest = { 
+            { EtatCase.Rouge, EtatCase.Jaune, EtatCase.Jaune, EtatCase.Jaune, EtatCase.Rouge, EtatCase.Rouge, EtatCase.Jaune },
+            { EtatCase.Jaune, EtatCase.Rouge, EtatCase.Rouge, EtatCase.Jaune, EtatCase.Rouge, EtatCase.Vide,  EtatCase.Jaune },
+            { EtatCase.Vide,  EtatCase.Vide,  EtatCase.Jaune,  EtatCase.Rouge,  EtatCase.Vide,  EtatCase.Vide,  EtatCase.Vide },
+            { EtatCase.Vide,  EtatCase.Vide,  EtatCase.Rouge,  EtatCase.Rouge,  EtatCase.Vide,  EtatCase.Vide,  EtatCase.Vide },
+            { EtatCase.Vide,  EtatCase.Vide,  EtatCase.Vide,  EtatCase.Vide,  EtatCase.Vide,  EtatCase.Vide,  EtatCase.Vide },
+            { EtatCase.Vide,  EtatCase.Vide,  EtatCase.Vide,  EtatCase.Vide,  EtatCase.Vide,  EtatCase.Vide,  EtatCase.Vide },
+        };
+
+
+        [Fact]
+        public void ConstructionNouveauJeux()
+        {
+            var partie = new Game();
+            Assert.NotNull(partie);
+        }
+
+        [Fact]
+        public void RepriseJeuInstanciation()
+        {
+            var partie = new Game(partieTest);
+            Assert.NotNull(partie);
+        }
+
+        [Fact]
+        public void TestCase()
+        {
+            var partie = new Game(partieTest);
+            for (int i = 0; i < 6; i++)
+                for (int j = 0; j < 7; j++)
+                    Assert.Equal(partie.EtatCase(j, i), partieTest[i, j]);
+        }
+
+        [Fact]
+        public void TestVainqueur()
+        {
+            var nouvellePartie = new Game();
+            Assert.Equal(EtatCase.Vide, nouvellePartie.Vainqueur());
+
+            var reprisePartie = new Game(partieTest);
+            Assert.Equal(EtatCase.Rouge, reprisePartie.Vainqueur());
+        }
+    }
+}
