4c88343bcffa671a22265ef928d4ea6dca6f74e2
Tarik <tarikbhm@gmail.com>
2020-06-18 19:50:14 +0200

    [junit Tests]

---
+++ src/test/java/cegedim/MainTest.java
@@ -0,0 +1,16 @@
+package cegedim;
+
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+import com.cegedim.test.model.Couleur;
+import com.cegedim.test.service.ServiceInitGame;
+
+public class MainTest {
+
+  @Test
+  public void test() {
+    ServiceInitGame game = new ServiceInitGame();
+    game.InitGrille();
+    assertEquals(game.getWinner().getCouleur(), Couleur.Rouge);
+  }
+}
